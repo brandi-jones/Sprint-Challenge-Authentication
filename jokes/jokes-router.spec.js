a21dec4b9487eefdd152ee4f513c365362120706
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-03-27 14:35:32 -0400

    completed additional testing for jokes-router.js just cause

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -4,30 +4,35 @@ const server = require("../api/server.js");
 
 
 describe("jokes-router.js", function() {
-    // it("test GET successful", async function() {
-    //     const userCreds = {
-    //         username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
-    //         password: "testPass"
-    //     }
-    //     const registerResponse = await request(server)
-    //                     .post("/api/auth/register")
-    //                     .send(userCreds)
+    it("test GET successful", async function() {
+        const userCreds = {
+            username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
+            password: "testPass"
+        }
+        const registerResponse = await request(server)
+                        .post("/api/auth/register")
+                        .send(userCreds)
 
-    //     const loginResponse = await request(server)
-    //                           .post("/api/auth/login")
-    //                           .send(userCreds)
+        const loginResponse = await request(server)
+                              .post("/api/auth/login")
+                              .send(userCreds)
 
-    //     const getResponse = await request(server)
-    //                         .get("/api/jokes")
-    //                         .set('Authorization', loginResponse.body.token)
+        const getResponse = await request(server)
+                            .get("/api/jokes")
+                            .set('Authorization', loginResponse.body.token)
 
 
-    //     const array = JSON.parse(getResponse.body);
-    //     console.log('array', array)
-    //     //expect(getResponse.body).toBe(expect.any(Array));
-    //     //expect(getResponse).toBe(expect.arrayContaining(expect.any(Object)));
-    //     expect(typeof array).toBe('array');
-    // })
+ 
+        // let array = JSON.stringify(getResponse.body);
+        // array = JSON.parse(array);
+        // // expect(typeof array).toBe('array');
+        // expect(array).toEqual(expect.arrayContaining({
+        //    "id": "0189hNRf2g", 
+        //    "joke": "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later."
+        // }))
+
+        expect(getResponse.status).toBe(200);
+    })
 
     it("test GET unsuccessful", async function() {
         const getResponse = await request(server)
