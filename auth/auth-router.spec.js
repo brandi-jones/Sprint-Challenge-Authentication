80cd199b0d3f1b1cf96549e72b3bb546c77182bf
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-03-27 13:26:12 -0400

    added testing for login functionality, and added more to register as well

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -25,7 +25,7 @@ describe("auth-router.js", function() {
             expect(response.status).toBe(201);
         })
 
-        it("should return obj containing onthe id and username of created user", async function() {
+        it("should return obj containing the id and username of created user", async function() {
             const newUser = {
                 username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
                 password: "testPass"
@@ -40,6 +40,72 @@ describe("auth-router.js", function() {
                 username: newUser.username
             })
             
+        })
+        
+        it("should return JSON", async function() {
+            const newUser = {
+                username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
+                password: "testPass"
+            }
+
+            const response = await request(server)
+                            .post("/api/auth/register")
+                            .send(newUser)
+
+            expect(response.type).toMatch(/json/i);
+        })
+    })
+
+    describe("login functionality", function () {
+        it("should return status 200 upon successful login", async function() {
+            const userCreds = {
+                username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
+                password: "testPass"
+            }
+            const registerResponse = await request(server)
+                            .post("/api/auth/register")
+                            .send(userCreds)
+
+            const loginResponse = await request(server)
+                                  .post("/api/auth/login")
+                                  .send(userCreds)
+
+            expect(loginResponse.status).toBe(200);
+        })
+
+        it("should return a token if successful", async function() {
+            const userCreds = {
+                username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
+                password: "testPass"
+            }
+            const registerResponse = await request(server)
+                            .post("/api/auth/register")
+                            .send(userCreds)
+
+            const loginResponse = await request(server)
+                                  .post("/api/auth/login")
+                                  .send(userCreds)
+            
+            expect(loginResponse.body).toStrictEqual({
+                token: expect.any(String)}
+            )})
+        it("should return wrong creds message if creds are incorrect", async function() {
+            let userCreds = {
+                username: `${Math.floor(Math.random() * 10000)}`, //create a random generated username ID, since ID must be unique for each entry of user
+                password: "testPass"
+            }
+            const registerResponse = await request(server)
+                            .post("/api/auth/register")
+                            .send(userCreds)
+
+            userCreds.password = "testChanged"; //change pass to wrong credentials to attempt a login with those creds
+
+            const loginResponse = await request(server)
+                                  .post("/api/auth/login")
+                                  .send(userCreds)
+            expect(loginResponse.body).toStrictEqual({
+                message: "You shall not pass! Wrong credentials!" 
+            })
         })
     })
 })
